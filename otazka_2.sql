2595622a211bbb8df4766a020797aeadbdd2ef85
Tomáš Havelec <havelect@gmail.com>
2025-11-26 14:26:38 +0100

    Update otazka_2.sql

--- otazka_2.sql
+++ otazka_2.sql
@@ -1,8 +1,8 @@
 -- Kolik je možné si koupit litrů mléka a kilogramů chleba za první a poslední rok?
 SELECT 
     rok,
-    ROUND(litru_mlika, 1) AS litru_mlika_za_mzdu,
-    ROUND(kg_chleba, 1) AS kg_chleba_za_mzdu
+    ROUND(litru_mlika_za_mzdu, 1),
+    ROUND(kg_chleba_za_mzdu, 1)
 FROM t_tomas_havelec_project_sql_primary_final
 WHERE rok IN (
     (SELECT MIN(rok) FROM t_tomas_havelec_project_sql_primary_final),
