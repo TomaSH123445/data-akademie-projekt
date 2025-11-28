9b1cc6b62f38f339b63c95cda0562e11db224122
Tomáš Havelec <havelect@gmail.com>
2025-11-28 13:01:38 +0100

    Rename selected columns for clarity in SQL query

--- otazka_2.sql
+++ otazka_2.sql
@@ -1,8 +1,8 @@
 -- Kolik je možné si koupit litrů mléka a kilogramů chleba za první a poslední rok?
 SELECT 
     rok,
-    ROUND(litru_mlika_za_mzdu, 1),
-    ROUND(kg_chleba_za_mzdu, 1)
+    ROUND(litru_mlika_za_mzdu, 1) AS počet_l,
+    ROUND(kg_chleba_za_mzdu, 1) AS počet_kg
 FROM t_tomas_havelec_project_sql_primary_final
 WHERE rok IN (
     (SELECT MIN(rok) FROM t_tomas_havelec_project_sql_primary_final),
